efe665456e736fe2fc90f0d3fe199bf09bf7a772
Bikcode <bikcodeh@gmail.com>
2022-10-13 01:24:23 -0500

    finish to implements unit test to detail view model

--- app/src/test/java/com/bikcodeh/melichallenge/ui/screens/detail/DetailViewModelTest.kt
+++ app/src/test/java/com/bikcodeh/melichallenge/ui/screens/detail/DetailViewModelTest.kt
@@ -3,13 +3,21 @@ package com.bikcodeh.melichallenge.ui.screens.detail
 import com.bikcodeh.melichallenge.core_test.util.CoroutineRule
 import com.bikcodeh.melichallenge.domain.usecase.GetProductDescriptionUseCase
 import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
 import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit4.MockKRule
+import io.mockk.slot
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import com.bikcodeh.melichallenge.domain.common.Result
+import com.bikcodeh.melichallenge.domain.model.ProductDescription
+import io.mockk.coVerify
 
 @ExperimentalCoroutinesApi
 class DetailViewModelTest {
@@ -24,6 +32,8 @@ class DetailViewModelTest {
     private lateinit var getProductDescriptionUseCase: GetProductDescriptionUseCase
 
     private lateinit var detailViewModel: DetailViewModel
+
+    private val slot = slot<String>()
 
     @Before
     fun setUp() {
@@ -44,6 +54,83 @@ class DetailViewModelTest {
     }
 
     @Test
-    fun getProductDescription() {
+    fun `getProductDescription should be successful`() = runTest {
+        /** Given */
+        coEvery { getProductDescriptionUseCase(capture(slot)) } returns Result.Success(
+            ProductDescription("test")
+        )
+        val results = arrayListOf<ProductDescriptionUiState>()
+
+        val job = launch(UnconfinedTestDispatcher()) {
+            detailViewModel.productDescriptionState.toList(results)
+        }
+
+        /** When */
+        detailViewModel.getProductDescription("1")
+
+        /** Then */
+        assertThat(results[0].isLoading).isFalse()
+        assertThat(results[1].isLoading).isTrue()
+        assertThat(results[1].hasError).isFalse()
+        assertThat(results[2].description).isEqualTo("test")
+        assertThat(results[2].isLoading).isFalse()
+        assertThat(results[2].hasError).isFalse()
+
+        coVerify(exactly = 1) { getProductDescriptionUseCase("1") }
+        job.cancel()
+    }
+
+    @Test
+    fun `getProductDescription should be an error`() = runTest {
+        /** Given */
+        coEvery { getProductDescriptionUseCase(capture(slot)) } returns Result.Error(
+            401, "error"
+        )
+        val results = arrayListOf<ProductDescriptionUiState>()
+
+        val job = launch(UnconfinedTestDispatcher()) {
+            detailViewModel.productDescriptionState.toList(results)
+        }
+
+        /** When */
+        detailViewModel.getProductDescription("1")
+
+        /** Then */
+        assertThat(results[0].isLoading).isFalse()
+        assertThat(results[1].isLoading).isTrue()
+        assertThat(results[1].hasError).isFalse()
+        assertThat(results[2].description).isEqualTo("")
+        assertThat(results[2].isLoading).isFalse()
+        assertThat(results[2].hasError).isTrue()
+
+        coVerify(exactly = 1) { getProductDescriptionUseCase("1") }
+        job.cancel()
+    }
+
+    @Test
+    fun `getProductDescription should be an exception`() = runTest {
+        /** Given */
+        coEvery { getProductDescriptionUseCase(capture(slot)) } returns Result.Exception(
+            Exception()
+        )
+        val results = arrayListOf<ProductDescriptionUiState>()
+
+        val job = launch(UnconfinedTestDispatcher()) {
+            detailViewModel.productDescriptionState.toList(results)
+        }
+
+        /** When */
+        detailViewModel.getProductDescription("1")
+
+        /** Then */
+        assertThat(results[0].isLoading).isFalse()
+        assertThat(results[1].isLoading).isTrue()
+        assertThat(results[1].hasError).isFalse()
+        assertThat(results[2].description).isEqualTo("")
+        assertThat(results[2].isLoading).isFalse()
+        assertThat(results[2].hasError).isTrue()
+
+        coVerify(exactly = 1) { getProductDescriptionUseCase("1") }
+        job.cancel()
     }
 }
