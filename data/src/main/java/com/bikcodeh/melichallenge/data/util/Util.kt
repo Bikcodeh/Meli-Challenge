aa760eb7ab2ff2eba01c9f6e4e036f71dcfcf2f1
Bikcode <bikcodeh@gmail.com>
2022-10-13 10:35:56 -0500

    implements unit test for converts util

--- data/src/main/java/com/bikcodeh/melichallenge/data/util/Util.kt
+++ data/src/main/java/com/bikcodeh/melichallenge/data/util/Util.kt
@@ -1,8 +1,9 @@
 package com.bikcodeh.melichallenge.data.util
 
+import com.squareup.moshi.JsonDataException
 import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okio.IOException
 
 object Util {
     inline fun <reified T> toJson(item: T): String {
@@ -14,6 +15,14 @@ object Util {
     inline fun <reified T> fromJson(json: String): T? {
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
         val jsonAdapter = moshi.adapter(T::class.java).lenient()
-        return jsonAdapter.fromJson(json)
+        return try {
+            jsonAdapter.fromJson(json)
+        } catch (e: JsonDataException) {
+            null
+        } catch (e: IOException) {
+            null
+        } catch (e: Exception) {
+            null
+        }
     }
 }
